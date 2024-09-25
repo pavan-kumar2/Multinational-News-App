baea624f05b233b5f4af436a9d1758f41b0872be
Pavan Kumar <pavankkarkera480@gmail.com>
2024-09-25 11:01:13 +0530

    ng-select ng-option dropdown is added.

--- src/app/components/dropdown/dropdown.component.ts
+++ src/app/components/dropdown/dropdown.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./dropdown.component.scss']
 })
 export class DropdownComponent {
+  selectedCar!: number;
 
+  cars = [
+    { id: 1, name: 'Volvo' },
+    { id: 2, name: 'Saab' },
+    { id: 3, name: 'Opel' },
+    { id: 4, name: 'Audi' },
+  ];
 }
