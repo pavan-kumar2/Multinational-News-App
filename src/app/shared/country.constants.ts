84d3dbc18a050b04741331233766ac903359a93e
Pavan Kumar <pavankkarkera480@gmail.com>
2024-10-11 21:58:22 +0530

    COUNTRIES_KEY_VALUE is created for header name Title.

--- src/app/shared/country.constants.ts
+++ src/app/shared/country.constants.ts
@@ -1,15 +1,30 @@
-export const SELECTED_COUNTRY = 'india';
 
 export const COUNTRIES = [
+    { value: 'india', name: 'India' },
+    { value: 'uk', name: 'United Kingdom' },
+    { value: 'usa', name: 'United States' },
     { value: 'australia', name: 'Australia' },
     { value: 'canada', name: 'Canada' },
     { value: 'france', name: 'France' },
     { value: 'germany', name: 'Germany' },
-    { value: 'india', name: 'India' },
     { value: 'italy', name: 'Italy' },
-    { value: 'newZealand', name: 'NewZealand' },
-    { value: 'saudiArabia', name: 'SaudiArabia' },
+    { value: 'newZealand', name: 'New Zealand' },
+    { value: 'saudiArabia', name: 'Saudi Arabia' },
     { value: 'singapore', name: 'Singapore' },
-    { value: 'uk', name: 'UK' },
-    { value: 'usa', name: 'USA' },
 ]
+
+export const SELECTED_COUNTRY = COUNTRIES[0].value;
+
+export const COUNTRIES_KEY_VALUE: { [key: string]: string } = {
+    india: 'India',
+    uk: 'United Kingdom',
+    usa: 'United States',
+    australia: 'Australia',
+    canada: 'Canada',
+    france: 'France',
+    germany: 'Germany',
+    italy: 'Italy',
+    newZealand: 'New Zealand',
+    saudiArabia: 'Saudi Arabia',
+    singapore: 'Singapore'
+}
