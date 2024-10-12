64486e13d01ab66381481c3a2697dc0aeef9ca01
Pavan Kumar <pavankkarkera480@gmail.com>
2024-10-12 21:29:55 +0530

    code improvement.

--- src/app/components/cards/cards.component.ts
+++ src/app/components/cards/cards.component.ts
@@ -1,33 +1,31 @@
 
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { NewsArticle } from 'src/app/shared/news-articles.model';
 import { NewsApiService } from 'src/app/services/news-api.service';
+import { Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-cards',
   templateUrl: './cards.component.html',
   styleUrls: ['./cards.component.scss'],
 })
-export class CardsComponent {
-  newsArticles: NewsArticle[] = [];
+export class CardsComponent implements OnInit, OnDestroy {
 
+  newsArticles: NewsArticle[] = [];
   isError: boolean = false;
-  isLoading: boolean = false;
+  isLoading: boolean = true;
 
   emptyStateImage = 'assets/images/no-image-found.png';
 
   private _selectedCountry!: string;
 
-  @Input() set selectedCountry(country) {
+  private destroy$ = new Subject<void>();
 
+  @Input() set selectedCountry(country: string) {
+    this.isLoading = true;
     if (country) {
       this._selectedCountry = country;
-
-      this.newsApiService
-        .getNewsRequest(this.selectedCountry)
-        .subscribe(value => this.newsArticles = value);
-      this.newsApiService.isError$.subscribe(value => this.isError = value);
-      this.newsApiService.isLoading$.subscribe(value => this.isLoading = value)
+      this.fetchNewsArticles(this._selectedCountry);
     }
 
   }
@@ -37,18 +35,27 @@ export class CardsComponent {
   }
 
 
-
-
   constructor(
     private newsApiService: NewsApiService
   ) { }
 
   ngOnInit() {
+    // Subscribe to the loading and error observables once when the component is initialized
+    this.newsApiService.isLoading$
+      .pipe(takeUntil(this.destroy$))
+      .subscribe(value => (this.isLoading = value));
 
-
+    this.newsApiService.isError$
+      .pipe(takeUntil(this.destroy$))
+      .subscribe(value => (this.isError = value));
   }
 
-
+  fetchNewsArticles(country: string) {
+    this.newsApiService
+      .getNewsRequest(country)
+      .pipe(takeUntil(this.destroy$))
+      .subscribe(value => this.newsArticles = value);
+  }
 
   imageErrorHandling(event: Event) {
     let imageElement = event.target as HTMLImageElement;
@@ -58,5 +65,10 @@ export class CardsComponent {
   // create dummy array for loading shimmer
   createArray(length: number): number[] {
     return Array.from({ length }, (_, i) => i)
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 }
--- src/app/services/news-api.service.ts
+++ src/app/services/news-api.service.ts
@@ -9,7 +9,7 @@ import { BehaviorSubject, catchError, map, Observable, of, tap } from 'rxjs';
 export class NewsApiService {
 
   private isErrorSubject: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
-  private isLoadingSubject: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(true);
+  private isLoadingSubject: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
 
   isError$: Observable<boolean> = this.isErrorSubject.asObservable();
   isLoading$: Observable<boolean> = this.isLoadingSubject.asObservable();
@@ -27,31 +27,45 @@ export class NewsApiService {
 
   updatedDate = `${this.formateDate}   <span>${this.formateTime}</span>`
 
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient) { }
+
+  private getFormat1tedDate() {
+    const currentDate = new Date();
+    const formateDate = currentDate.toLocaleDateString('en-CA', {
+      year: 'numeric',
+      month: '2-digit',
+      day: '2-digit'
+    })
+
+    const formateTime = this.currentDate.toLocaleTimeString('en-US', { hour12: true })
+
+    return `${formateDate}   <span>${formateTime}</span>`;
 
   }
 
   getNewsRequest(country: string) {
+    this.isLoadingSubject.next(true);
     return this.http.get<{ articles: NewsArticle[] }>(`https://gist.githubusercontent.com/pavan-kumar2/8fb68c793eda5c22a75492593a4572d5/raw/80516d649f9b308bad93dc265827ff0d7479cce8/multinational-news-${country}.json`)
       .pipe(
-        map(val => val.articles
-          .map(article => {
+        map(response => {
+          return response.articles.map(article => {
             return {
               ...article,
-              urlToImage: article.urlToImage ? article.urlToImage : 'assets/images/no-image-found.png',
-              publishedAt: this.updatedDate
+              urlToImage: article.urlToImage || 'assets/images/no-image-found.png',
+              publishedAt: this.getFormat1tedDate()
             }
-          })),
+          })
+        }),
         tap(() => {
           this.isLoadingSubject.next(false)
           this.isErrorSubject.next(false)
         }),
         catchError(error => {
+          console.error('Error fetching news:', error);
           this.isLoadingSubject.next(false);
           this.isErrorSubject.next(true);
           return of([])
         }),
-
-      )
+      );
   }
 }
