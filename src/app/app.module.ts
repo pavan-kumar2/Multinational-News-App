888a0e35a756a0795c788d871a9fb0f575503429
Pavan Kumar <pavankkarkera480@gmail.com>
2024-10-20 14:41:01 +0530

    hydration implement.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -18,14 +18,14 @@ import { LoadingShimmerCardComponent } from "./components/loading-shimmer-card/l
     CardsComponent
   ],
   imports: [
-    BrowserModule,
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
     AppRoutingModule,
     NgSelectModule,
     FormsModule,
     HttpClientModule,
     LoadingShimmerCardComponent
   ],
-  providers: [],
+  providers: [provideClientHydration()],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
